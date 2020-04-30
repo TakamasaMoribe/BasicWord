9f029808dbca30b44f3d4fc32cd17f371e898e8b
森部高昌 <takamasamoribe@gmail.com>
2020-04-30 11:42:08 +0900

    update  add a file
    QuestionDataManager.swift 追加
    04/30 1140

--- BasicWord/QuestionDataManager.swift
+++ BasicWord/QuestionDataManager.swift
@@ -11,7 +11,8 @@ import UIKit
 //１つの問題に関する情報
 class QuestionData {
     
-    var questionNo:String     //問題番号 固有の番号　ｃｓｖファイルに記載されている
+    //ｃｓｖファイルから取り出すデータ
+    var originNo:String       //問題番号 固有の番号
     var question:String       //問題文
     var correctAnswer:String  //正解
     var answer1:String        //選択肢１
@@ -19,11 +20,14 @@ class QuestionData {
     var answer3:String        //選択肢３
     var answer4:String        //選択肢４
     
-    var userChoiceAnswer:String = "" //ユーザーが選択した答
+    //プログラム実行中に取得するデータ
+    var userChoiceAnswer:String? // = "" //ユーザーが選択した答
+    var questionNo:Int = 0           //問題の番号
+    var correctCount:Int = 0      //ユーザーが正解した数???????????
     
-    
+    //イニシャライザー　配列questionSourceDataArrayを受け取ることができる
     init(questionSourceDataArray:[String]) {
-        questionNo = questionSourceDataArray[0]    //問題番号
+        originNo = questionSourceDataArray[0]      //問題番号
         question = questionSourceDataArray[1]      //問題文
         correctAnswer = questionSourceDataArray[2] //正解
         answer1 = questionSourceDataArray[3]       //選択肢１
@@ -32,16 +36,18 @@ class QuestionData {
         answer4 = questionSourceDataArray[6]       //選択肢４
     }
     
-    //正誤の判定をして、Bool値を返す
+    //正誤の判定をして、Bool値を返す　Boolではなく、Intで返せば、正解数の保持は可能？
     func isCorrect() -> Bool {
         if correctAnswer == userChoiceAnswer {
-            return true
+            correctCount += 1 //正解の数を１つ増やす？？どこでどうやって使う
+            return true//正解
         }
-        return false
+        return false   //不正解
     }
     
 }
 // end of class QuestionData =============================================
+
 
 class QuestionDataManeger {
         
@@ -54,9 +60,9 @@ class QuestionDataManeger {
     var questionDataArray = [QuestionData]()
     
     //現在の問題のインデックス
-    var nowQuestionIndex:Int = 0  //現在の問題のインデックス
-    var correctCount:Int = 0      //ユーザーが正解した数
-    //シングルトンであることを保証するため
+    var nowQuestionIndex:Int = 0  //問題文配列のインデックス
+
+    //シングルトンであることを保証するために初期化する
     private init(){
     }
     
@@ -71,39 +77,43 @@ class QuestionDataManeger {
         //問題ファイルのパスを指定する　セクメンティッドコントロールから取得する
         guard let csvFilePath = Bundle.main.path(forResource: filename, ofType: "csv") else {
             print("ファイルが存在しません")//エラー処理が欲しい
-            
             return
         }
-        //問題ファイルからデータを読み込む
+        //CSV問題ファイルからデータを読み込む
         //クロージャ:関数の実行結果を次の処理で続けて使用する関数
         //enumerateLinesは改行（\n (バックスラッシュ + n))単位で文字列を読み込むメソッド
         //stopはそのままの意味でstop変数にtrueを代入した時にループが終了する
         //lineやstopは決められた名前ではなく、自分の好きな名前を付けられる
         do {
             let csvStringData = try String(contentsOfFile: csvFilePath,encoding: String.Encoding.utf8)
+            
             csvStringData.enumerateLines(invoking: {(line,stop) in //改行されるごとに分割する
                 let questionSourceDataArray = line.components(separatedBy: ",") //１行を","で分割して配列に入れる
                 let questionData = QuestionData(questionSourceDataArray: questionSourceDataArray)//１行分の配列
-                self.questionDataArray.append(questionData) //格納用の配列に、１行ずつ追加していく
+                    //QuestionDataクラスのインスタンスとして、１つの問題文を入れる
+                self.questionDataArray.append(questionData) //格納用の配列に、１行（１つの問題文）ずつ追加していく
+                //問題番号を設定 =questionDataArrayに追加した順番を表す
+                questionData.questionNo = self.questionDataArray.count
                 }) //invokingからのクロージャここまで
             
             }catch let error {
                 print("ファイル読み込みエラー:\(error)")
                 return
-        } //do節ここまで
-    //問題の出題順をシャッフルする　配列内で要素をシャッフルする
-    questionDataArray.shuffle() //これだけでOK
+            } //do節ここまで
+        //問題の出題順をシャッフルする　配列内で要素をシャッフルする
+        questionDataArray.shuffle() //これだけでOK
     }
-
+  // end of func loadQuestion() -----------------------------------------
     
     //問題文の取り出し  QuestionDataManeger.sharedInstance.nextQuestion() ****
+    
     func nextQuestion() -> QuestionData? {
         if nowQuestionIndex < questionDataArray.count { //問題に残りがある時
             let nextQuestion = questionDataArray[nowQuestionIndex]
-            nowQuestionIndex += 1 //次の問題へ
-            return nextQuestion
+            nowQuestionIndex += 1
+            return nextQuestion //次の問題へ
         }
-        return nil
+        return nil //全部解き終わって、次の問題がない時
     }
  
 }
